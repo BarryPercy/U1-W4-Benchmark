f8e4ffff7dd969bd024acc9b0972bee83be5e5bd
Barry Percy <baz.percy@gmail.com>
2022-12-13 14:45:33 +0000

    Added behind ring

--- js/timer.js
+++ js/timer.js
@@ -1,4 +1,4 @@
-document.getElementById("timer").innerHTML = `
+/*document.getElementById("timer").innerHTML = `
 <div class="base-timer">
   <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
     <g class="base-timer__circle">
@@ -6,6 +6,29 @@ document.getElementById("timer").innerHTML = `
     </g>
   </svg>
   <span class="timer-value base-timer__label" span id="base-timer-label">
+  </span>
+</div>
+`;*/
+
+document.getElementById("timer").innerHTML = `
+<div class="base-timer">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+      <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
+    </g>
+  </svg>
+  <span id="base-timer-label" class="base-timer__label timer-value">
   </span>
 </div>
 `;
@@ -20,14 +43,41 @@ let timerInterval = null;
 function startTimer() {
   timerInterval = setInterval(() => {
     if(timeLeft===0){
-        nextQuestion();
-        return;
+      nextQuestion();
+      return;
     }
     timePassed = timePassed += 1;
     timeLeft = TIME_LIMIT - timePassed;
-    document.getElementById("base-timer-label").innerHTML = timeLeft
+    document.getElementById("base-timer-label").innerHTML = timeLeft;
+    
+    setCircleDasharray();
   }, 1000);
 }
+
+function calculateTimeFraction() {
+  return timeLeft / TIME_LIMIT;
+}
+    
+// Update the dasharray value as time passes, starting with 283
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
+
+
+
+
+
+
+
+
+
+
+
 
 function nextQuestion(){
     console.log("go next question here");
