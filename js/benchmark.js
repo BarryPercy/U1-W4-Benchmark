1a6928c38c713045e0ae751f69b15d6cda316264
ajeer333 <ajeer333@gmail.com>
2022-12-13 21:54:17 +0100

    displaying options partially

--- js/benchmark.js
+++ js/benchmark.js
@@ -5,10 +5,11 @@ const questions = [
       difficulty: "easy",
       question: "What does CPU stand for?",
       correct_answer: "Central Processing Unit",
-      incorrect_answers: [
+      options: [
         "Central Process Unit",
         "Computer Personal Unit",
         "Central Processor Unit",
+        "Central Processing Unit"
       ],
     },
     {
@@ -18,7 +19,7 @@ const questions = [
       question:
         "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
       correct_answer: "Final",
-      incorrect_answers: ["Static", "Private", "Public"],
+      options: ["Static", "Private", "Public","Final"],
     },
     {
       category: "Science: Computers",
@@ -26,7 +27,7 @@ const questions = [
       difficulty: "easy",
       question: "The logo for Snapchat is a Bell.",
       correct_answer: "False",
-      incorrect_answers: ["True"],
+      options: ["True","False"],
     },
     {
       category: "Science: Computers",
@@ -35,7 +36,7 @@ const questions = [
       question:
         "Pointers were not used in the original C programming language; they were added later on in C++.",
       correct_answer: "False",
-      incorrect_answers: ["True"],
+      options: ["True","False"],
     },
     {
       category: "Science: Computers",
@@ -44,7 +45,7 @@ const questions = [
       question:
         "What is the most preferred image format used for logos in the Wikimedia database?",
       correct_answer: ".svg",
-      incorrect_answers: [".png", ".jpeg", ".gif"],
+      options: [".png", ".jpeg", ".gif",".svg"],
     },
     {
       category: "Science: Computers",
@@ -52,10 +53,11 @@ const questions = [
       difficulty: "easy",
       question: "In web design, what does CSS stand for?",
       correct_answer: "Cascading Style Sheet",
-      incorrect_answers: [
+      options: [
         "Counter Strike: Source",
         "Corrective Style Sheet",
         "Computer Style Sheet",
+        "Cascading Style Sheet"
       ],
     },
     {
@@ -65,10 +67,11 @@ const questions = [
       question:
         "What is the code name for the mobile operating system Android 7.0?",
       correct_answer: "Nougat",
-      incorrect_answers: [
+      options: [
         "Ice Cream Sandwich",
         "Jelly Bean",
         "Marshmallow",
+        "Nougat"
       ],
     },
     {
@@ -77,7 +80,7 @@ const questions = [
       difficulty: "easy",
       question: "On Twitter, what is the character limit for a Tweet?",
       correct_answer: "140",
-      incorrect_answers: ["120", "160", "100"],
+      options: ["120", "160", "100","140"],
     },
     {
       category: "Science: Computers",
@@ -85,7 +88,7 @@ const questions = [
       difficulty: "easy",
       question: "Linux was first created as an alternative to Windows XP.",
       correct_answer: "False",
-      incorrect_answers: ["True"],
+      options: ["True","False"],
     },
     {
       category: "Science: Computers",
@@ -94,19 +97,47 @@ const questions = [
       question:
         "Which programming language shares its name with an island in Indonesia?",
       correct_answer: "Java",
-      incorrect_answers: ["Python", "C", "Jakarta"],
+      options: ["Python", "C", "Jakarta","Java"],
     },
   ];
 
 
   let totalScore=0;
   let currentIndex=-1;
+  let optionIndex=-1
 function displayQuestion(){   
         let question = document.getElementById("question");
         currentIndex++;
             let currentQuestion = questions[currentIndex];
             question.innerHTML=currentQuestion.question;
-            /*let nextButtonNode=document.getElementById('next-btn');
+           
+}
+
+   
+function displayOptions(){
+    
+    optionIndex=optionIndex+1;
+    let currentQuestion = questions[optionIndex];
+    let currentOptions=currentQuestion.options
+    let answer=document.getElementById('answer-btns');
+    for(i=0;i<currentQuestion.options.length;i++){
+        let option=document.createElement('button')
+        option.classList.add('answer-btn')
+        option.innerText=currentQuestion.options[i]
+        answer.appendChild(option)
+    }
+    
+    
+    
+}
+
+
+
+
+
+
+
+/*let nextButtonNode=document.getElementById('next-btn');
              nextButtonNode.addEventListener("click",function(){
                             currentIndex=currentIndex+1;
                              displayQuestion();
@@ -114,7 +145,7 @@ function displayQuestion(){
                              
                  })*/
                  
-            }
+            
 
 
  
@@ -140,6 +171,7 @@ function selectAnswer(){
 
     function startGame(){
         displayQuestion()
+        displayOptions()
        // selectAnswer()
     }
 
